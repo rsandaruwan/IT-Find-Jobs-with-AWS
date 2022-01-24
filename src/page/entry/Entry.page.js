68ca0e28ecbb27ca9d5b21e072ce33717a73fad4
Ravindu Sandaruwan <48220341+rsandaruwan@users.noreply.github.com>
2022-01-25 01:25:50 +0530

    Start dev frontend

---
+++ src/page/entry/Entry.page.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./entry.style.css";
+import {Jumbotron} from "react-bootstrap";
+
+
+export const Entry = ()=>{
+    return(
+        <div className="entry-page bg-info">
+            <Jumbotron className="jumbotron">
+                Hello fff
+            </Jumbotron>
+        </div>
+    )
+}
